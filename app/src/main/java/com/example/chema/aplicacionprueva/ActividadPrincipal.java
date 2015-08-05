69346e38cfa32635ae743d7433b6041f446e6963
chemicla1 <josemanuel.gomez.santos@gmail.com>
2015-08-05 16:56:02 +0200

    HEY HEY

--- app/src/main/java/com/example/chema/aplicacionprueva/ActividadPrincipal.java
+++ app/src/main/java/com/example/chema/aplicacionprueva/ActividadPrincipal.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class ActividadPrincipal extends AppCompatActivity {
-
+String chema="hola";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
